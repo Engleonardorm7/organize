c10e4516ff83bfb67d1d4ce6fca0147e6c86d859
Leonardo Rodriguez <leonardorm7@hotmail.com>
2022-08-22 21:09:52 -0500

    working well

--- arrange.cpp
+++ arrange.cpp
@@ -17,17 +17,13 @@ int main()
    int num;
 
 for(int i=0;i<sizeof(list);i++){
+	num=list[i];
 	
-	for(int k=0;k<i;k++){
-		for(int j=1;j<i;j++){
-			if(list[j]<list[j-1]){
-				num=list[j];
-				list[j]=list[j-1];
-				list[j-1]=num;
-			}
-
-		}
+	while((i>0)&&list[i-1]>num){
+		list[i]=list[i-1];
+		i--;
 	}
+	list[i]=num;
 }
 	for(int j=0;j<sizeof(list);j++){
 	cout<<(list[j]);
